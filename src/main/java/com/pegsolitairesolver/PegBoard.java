73768aa8d4883963b3b19f39df6d7bf4405f9850
ndrw-ynz <andrewyanza4@gmail.com>
2023-01-14 21:58:01 +0800

    Added utility methods for PegBoard

--- src/main/java/com/pegsolitairesolver/PegBoard.java
+++ src/main/java/com/pegsolitairesolver/PegBoard.java
@@ -1,12 +1,13 @@
 package com.pegsolitairesolver;
 
-import javafx.scene.canvas.Canvas;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * PegBoard is a class containing all the state and behavior of the
  * peg board of the GUI application.
  */
-public class PegBoard extends Canvas {
+public class PegBoard{
     /**The integer value of an invalid piece.*/
     private static final int INVALID = 0;
     /**The integer value of an occupied piece.*/
@@ -24,17 +25,62 @@ public class PegBoard extends Canvas {
             {0, 0, 1, 1, 1, 0, 0}
     };
 
-    public void putPeg(int row, int col) {
-        this.board[row][col] =  OCCUPIED;
+    private final int[][] clearBoard = new int[][] {
+            {0, 0, 2, 2, 2, 0, 0},
+            {0, 0, 2, 2, 2, 0, 0},
+            {2, 2, 2, 2, 2, 2, 2},
+            {2, 2, 2, 1, 2, 2, 2},
+            {2, 2, 2, 2, 2, 2, 2},
+            {0, 0, 2, 2, 2, 0, 0},
+            {0, 0, 2, 2, 2, 0, 0}
+    };
+
+
+    public PegBoard() {
+
     }
 
-    public void setClear(int row, int col) {
-        this.board[row][col] = EMPTY;
+    private boolean isValidMove(int sourceRow, int sourceCol, int targetRow, int targetCol) {
+        return sourceRow >= 0 && sourceRow < board.length
+                && sourceCol >= 0 && sourceCol < board[sourceRow].length
+                && targetRow >= 0 && targetRow < board.length
+                && targetCol >= 0 && targetCol < board[targetRow].length
+                && board[targetRow][targetCol] == EMPTY
+                && board[(sourceRow + targetRow)/2][(sourceCol + targetCol)/2] == OCCUPIED
+                && board[sourceRow][sourceCol] == OCCUPIED;
     }
 
-    public boolean isValidMove(int sourceRow, int sourceCol, int targetRow, int targetCol) {
-        return false;
+    public ArrayList<ArrayList<Integer>> getValidMoves(int row, int col) {
+        ArrayList<ArrayList<Integer>> validMoves = new ArrayList<>();
+        if (isValidMove(row, col, row-2, col)) validMoves.add(new ArrayList<>(Arrays.asList(row-2, col)));
+        if (isValidMove(row, col, row+2, col)) validMoves.add(new ArrayList<>(Arrays.asList(row+2, col)));
+        if (isValidMove(row, col, row, col-2)) validMoves.add(new ArrayList<>(Arrays.asList(row, col-2)));
+        if (isValidMove(row, col, row, col+2)) validMoves.add(new ArrayList<>(Arrays.asList(row, col+2)));
+        return validMoves;
     }
 
+    public void movePeg(int sourceRow, int sourceCol, int targetRow, int targetCol) {
+        if (isValidMove(sourceRow, sourceCol, targetRow, targetCol)) {
+            board[targetRow][targetCol] = OCCUPIED;
+            board[(sourceRow + targetRow)/2][(sourceCol + targetCol)/2] = EMPTY;
+            board[sourceRow][sourceCol] = EMPTY;
+        }
+    }
 
+    public boolean isBoardCleared() {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] == clearBoard[row][col]) return false;
+            }
+        }
+        return true;
+    }
+
+    public void isGameOver() {
+
+    }
+
+    public void displayBoard() {
+
+    }
 }
