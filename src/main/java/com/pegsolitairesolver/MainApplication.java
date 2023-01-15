c2c7fba6ba7fcb04379b1a68babcb5b0dfa6c1a3
ndrw-ynz <andrewyanza4@gmail.com>
2023-01-15 22:10:43 +0800

    Created pegBoard and modified start.

--- src/main/java/com/pegsolitairesolver/MainApplication.java
+++ src/main/java/com/pegsolitairesolver/MainApplication.java
@@ -3,9 +3,13 @@ package com.pegsolitairesolver;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
+
+    private PegBoard pegBoard;
+
     @Override
     public void start(Stage stage) {
         stage.setTitle("Peg Solitaire Solver");
@@ -16,8 +20,16 @@ public class MainApplication extends Application {
         Group root = new Group();
         Scene scene = new Scene(root);
 
-        PegBoard pegBoard = new PegBoard();
+        Text text = new Text(10, 10, "Peg Solitaire Solver");
+        root.getChildren().add(text);
 
+        this.pegBoard = new PegBoard();
+        Square[][] squares = pegBoard.getBoard();
+        for (int row = 0; row < 7; row++) {
+            for (int col = 0; col < 7; col++) {
+                root.getChildren().add(squares[row][col]);
+            }
+        }
         stage.setScene(scene);
         stage.show();
     }
