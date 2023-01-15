d54fa98f0a442ed3f6275fe72db4c2fd0af80635
ndrw-ynz <andrewyanza4@gmail.com>
2023-01-15 22:13:29 +0800

    Created row and col. Created and modified constructor for circle.

--- src/main/java/com/pegsolitairesolver/PegPiece.java
+++ src/main/java/com/pegsolitairesolver/PegPiece.java
@@ -1,10 +1,18 @@
 package com.pegsolitairesolver;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class PegPiece {
+public class PegPiece extends Circle {
 
+    private int row;
+    private int col;
     public PegPiece(int row, int col) {
-
+        this.row = row;
+        this.col = col;
+        this.setFill(Color.BLACK);
+        this.setRadius(15);
+        this.setCenterX(155+35*row);
+        this.setCenterY(70+35*col);
     }
 }
