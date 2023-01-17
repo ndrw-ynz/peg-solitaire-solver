4eadfe2c01d53daa46116715aebafa3a1d7b273f
ndrw-ynz <andrewyanza4@gmail.com>
2023-01-17 22:59:15 +0800

    Created row, col variables. Created getRow, getCol, getValidMoves, addValidMove, and resetValidMoves methods.

--- src/main/java/com/pegsolitairesolver/PegPiece.java
+++ src/main/java/com/pegsolitairesolver/PegPiece.java
@@ -3,10 +3,12 @@ package com.pegsolitairesolver;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class PegPiece extends Circle {
+import java.util.ArrayList;
 
-    private int row;
-    private int col;
+public class PegPiece extends Circle {
+    private final int row;
+    private final int col;
+    private final ArrayList<String> validMoves = new ArrayList<>();
     public PegPiece(int row, int col) {
         this.row = row;
         this.col = col;
@@ -14,5 +16,23 @@ public class PegPiece extends Circle {
         this.setRadius(15);
         this.setCenterX(155+35*row);
         this.setCenterY(70+35*col);
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public ArrayList<String> getValidMoves() {return validMoves;}
+
+    public void addValidMove(String move) {
+        this.validMoves.add(move);
+    }
+
+    public void resetValidMoves() {
+        this.validMoves.clear();
     }
 }
